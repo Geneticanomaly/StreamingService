a3404dbeb5c7fce76b6efb56c1261ad21dc749c7
otsoweckstrom <42272286+otsoweckstrom@users.noreply.github.com>
2022-12-17 17:50:23 +0200

    remove unused import

--- videostreaming-master/src/App.js
+++ videostreaming-master/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 import './App.css'
-import { Button, Box } from '@mui/material'
+import { Button } from '@mui/material'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
